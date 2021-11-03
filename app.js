e0fa521e6c9cf5f5ccc92c95fb74e94feacb2ddb
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2021-11-03 16:24:40 +0300

    project done

--- app.js
+++ app.js
@@ -23,9 +23,31 @@ function createServiceCardsSection(url) {
        `;
       container.append(cardDiv);
     });
-    return container;
+  });
+}
+
+function createProductCardsSection(url) {
+  let container = document.querySelector(".product-cards__list");
+  getDataFromJson(url).then((data) => {
+    data.forEach((el) => {
+      let cardDiv = document.createElement("div");
+      cardDiv.className = "product-card";
+      cardDiv.innerHTML = `
+      <a href="#">
+        <div class="product-card__foto">
+          <img src=${el.foto}>
+        </div>
+        <div class="product-card__title">${el.title}</div>
+        <div class="product-card__price">${el.price}</div>
+      </a>
+      `;
+      container.append(cardDiv);
+    });
   });
 }
 
 const urlServiceCards = "./json/serviceCards.json";
+const urlProductCards = "./json/productCards.json";
+
 createServiceCardsSection(urlServiceCards);
+createProductCardsSection(urlProductCards);
